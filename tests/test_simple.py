a1620606860fa9975553cbe657059b4af13db9a9
Ji Xin <xinji1996@gmail.com>
2022-12-17 19:40:02 -0500

    Add a test

---
+++ tests/test_simple.py
@@ -0,0 +1,9 @@
+from daam import trace, set_seed
+from diffusers import StableDiffusionPipeline
+from matplotlib import pyplot as plt
+import torch
+
+# just set up some imports and a dummy test
+
+def test_dummy():
+    assert 1 + 2 == 3
